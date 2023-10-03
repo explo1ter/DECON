09d4a34f517819972f655182558e3db110b85d34
explo1ter <145901303+explo1ter@users.noreply.github.com>
2023-10-03 23:50:52 +0530

    Update decon.py

--- decon.py
+++ decon.py
@@ -48,16 +48,49 @@ def decrypt_md5():
             stripped_hash = word.strip()
             if(hashlib.md5(stripped_hash.encode()).hexdigest() == md5_hash):
                 f = True
-                print(f"\nDecrypted!!\nDecoded string : {word}")
+                print(f"\nDecoded string : {word}")
                 break
     except:
         if f == False:
             print("\nDecoding failed\nuse another wordlist")
 
+def encrypt_sha256():
+    try:
+        sha_raw = input("\nEnter the string to encrypt : ")
+        sha_enc = hashlib.sha256(sha_raw.encode()).hexdigest()
+        print(f"\nEncrypted string : {sha_enc}")
+
+    except:
+        print("\nSomething went wrong")
+        encrypt_sha256()
+
+def decrypt_sha256():
+    f = False
+    sha_hash = input("Enter the md5 string to decode : ")
+
+    wordlist = input("Enter the wordlist(complete path should be mentioned) : ")
+    try:
+        wlist = open(wordlist, "r")
+    except:
+        print("wordlist not found")
+        decrypt_sha256()
+    print("Wordlist found")
+    try:
+        for word in wlist:
+            stripped_hash = word.strip()
+            if (hashlib.sha256(stripped_hash.encode()).hexdigest() == sha_hash):
+                f = True
+                print(f"\nDecoded string : {word}")
+                break
+    except:
+        if f == False:
+            print("Decoding failed\n use another wordlist")
+
+
 
 choice = 0
-while choice != 4:
-    print("\n**********MENU***********\n\n1.Base64\n2.MD5\n3.Exit")
+while choice != 5:
+    print("\n**********MENU***********\n\n1.Base64\n2.MD5\n3.SHA 256\n4.Exit")
     choice =int(input("Enter your choice : "))
     if choice == 1:
         option = 0
@@ -76,7 +109,7 @@ while choice != 4:
     elif choice == 2:
         option = 0
         while option != 4:
-            print("\n*******md5******\n1.Encryptor\n2.Decryptor\n3.Back")
+            print("\n*******md5******\n1.Encryptor\n2.Decoder\n3.Back")
             option = int(input("Enter your option : "))
             if option == 1:
                 encrypt_md5()
@@ -86,7 +119,22 @@ while choice != 4:
                 break
             else:
                 print("invalid choice")
+
     elif choice == 3:
+        option = 0
+        while option != 4:
+            print("\n*******SHA 256******\n1.Encryptor\n2.Decoder\n3.Back")
+            option = int(input("Enter your option : "))
+            if option == 1:
+                encrypt_sha256()
+            elif option == 2:
+                decrypt_sha256()
+            elif option == 3:
+                break
+            else:
+                print("invalid choice")
+
+    elif choice == 4:
         print("\nExiting\nThankYou use again :)")
         exit(0)
     else:
